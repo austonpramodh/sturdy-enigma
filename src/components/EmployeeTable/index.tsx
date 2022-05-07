b9040a094a7dc82fa8f77b906b720bca6dce02ac
Auston Pramodh Barboza <austonpramodh@gmail.com>
2022-05-07 12:30:09 -0400

    chore: added delete functionality

--- src/components/EmployeeTable/index.tsx
+++ src/components/EmployeeTable/index.tsx
@@ -31,46 +31,64 @@ const EmployeeTable: React.FunctionComponent<Props> = ({
             <TableCell align="right">Name</TableCell>
             <TableCell align="right">Email</TableCell>
             <TableCell align="right">Title</TableCell>
-            <TableCell align="right">Salary</TableCell>
+            <TableCell align="right">Gross Annual Salary</TableCell>
+            <TableCell align="right">Federal Tax@10%</TableCell>
+            <TableCell align="right">State Tax@5%</TableCell>
+            <TableCell align="right">Other Tax@3%</TableCell>
+            <TableCell align="right">In-hand Annual Salary</TableCell>
+            <TableCell align="right">In-hand Monthly Salary</TableCell>
             <TableCell align="right">Delete?</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {employees.map((row) => (
-            <TableRow
-              key={`${row.name}-${row.id}`}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              selected={isLoading}
-            >
-              <TableCell component="th" scope="row">
-                {row.id}
-              </TableCell>
-              <TableCell align="right">{row.name}</TableCell>
-              <TableCell align="right">{row.email}</TableCell>
-              <TableCell align="right">{row.title}</TableCell>
-              <TableCell align="right">{row.salary}</TableCell>
-              <TableCell align="center">
+          {employees.map((row) => {
+            const salary = row.salary
+            const fedTax = (salary / 100) * 10
+            const stateTax = (salary / 100) * 5
+            const otherTax = (salary / 100) * 3
+            const inHandSalary = salary - fedTax - stateTax - otherTax
+            const inHandMonthly = inHandSalary / 12
+            return (
+              <TableRow
+                key={`${row.name}-${row.id}`}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                selected={isLoading}
+              >
+                <TableCell component="th" scope="row">
+                  {row.id}
+                </TableCell>
+                <TableCell align="right">{row.name}</TableCell>
+                <TableCell align="right">{row.email}</TableCell>
+                <TableCell align="right">{row.title}</TableCell>
+                <TableCell align="right">{Math.round(row.salary * 100) / 100}</TableCell>
+                <TableCell align="right">{Math.round(fedTax * 100) / 100}</TableCell>
+                <TableCell align="right">{Math.round(stateTax * 100) / 100}</TableCell>
+                <TableCell align="right">{Math.round(otherTax * 100) / 100}</TableCell>
+                <TableCell align="right">{Math.round(inHandSalary * 100) / 100}</TableCell>
+                <TableCell align="right">{Math.round(inHandMonthly * 100) / 100}</TableCell>
+                <TableCell align="center">
 
-                <IconButton 
-                disabled={isLoading} 
-                onClick={() => {
-                  // delete this row
-                  setIsloading(true)
-                  axios.delete(`/api/${row.id}`).then((res)=>{
-                    console.log(res)
-                    setIsloading(false)
-                    // @ts-ignore
-                    router.reload(window.location.pathname)
-                  }).catch((e) => {
-                    console.log(e)
-                    setIsloading(false)
-                  })
-                }}>
-                  <DeleteForever />
-                </IconButton>
-              </TableCell>
-            </TableRow>
-          ))}
+                  <IconButton
+                    disabled={isLoading}
+                    onClick={() => {
+                      // delete this row
+                      setIsloading(true)
+                      axios.delete(`/api/${row.id}`).then((res) => {
+                        console.log(res)
+                        setIsloading(false)
+                        // @ts-ignore
+                        router.reload(window.location.pathname)
+                      }).catch((e) => {
+                        console.log(e)
+                        setIsloading(false)
+                      })
+                    }}>
+                    <DeleteForever />
+                  </IconButton>
+                </TableCell>
+              </TableRow>
+            )
+          })}
         </TableBody>
       </Table>
     </TableContainer>
